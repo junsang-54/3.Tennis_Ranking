72ec84d6a408f0b9ad6b6d2b3bf3744d5ace30d2
Jusang_ <0504won@gmail.com>
2021-05-26 21:37:22 +0900

    Update tennis_ranking.py

--- tennis_ranking.py
+++ tennis_ranking.py
@@ -1,24 +1,56 @@
+#라이브러리 설정
+import sys
+import sklearn
+import numpy as np
+import os
+
+% matplotlib
+inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+#변수 및 함수 설정
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "end_to_end_project"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("그림 저장:", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
+
 #데이터 불러오기
 import pandas as pd
-tennis = pd.read_csv("./atp_matches_winner.csv")
+
+tennis = pd.read_csv("/Users/jinchoi725/Desktop/atp_matches_winner.csv")
 tennis
 
 
-#데이터 정제 : column, nan 제거
-tennis_NoNan = (tennis.dropna(thresh=12))
-tennis_NoNan
 
-true_copy_tennis_NoNan = tennis_NoNan.copy()
+#데이터 정제 : column, nan 제거
+tennis_NoNan = (tennis.dropna(thresh=12)) #column의 데이터가 없는(nan값을 가진) row 제거
+tennis_NoNan
+true_copy_tennis_NoNan = tennis_NoNan.copy() #정제된 데이터 copy
 
 
 #nan 제거 검증
-true_copy_tennis_NoNan.isnull().sum()
-true_copy_tennis_NoNan.describe()
+true_copy_tennis_NoNan.isnull().sum() #nan값이 있는지 isnull함수로 확인하고, column별로 nan값의 갯수를 한눈에 보기 편하게 sum함수 사용
 
+true_copy_tennis_NoNan.describe() #데이터의 정보 요약
 
-# describeget_ipython().run_line_magic('matplotlib', 'inline')
 
 #정제된 데이터 히스토그램 보기
+%matplotlib inline
 import matplotlib.pyplot as plt
 true_copy_tennis_NoNan.hist(bins=100, figsize=(80, 60))
 save_fig("attribute_histogram_plots")
@@ -38,11 +70,11 @@ test_set["w_ace"].hist()
 
 
 # 서브에이스 카테고리 생성
-import numpy as np
 true_copy_tennis_NoNan["ace_cat"] = pd.cut(true_copy_tennis_NoNan["w_ace"],
                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                               labels=[1, 2, 3, 4,5])
 true_copy_tennis_NoNan["ace_cat"].value_counts()
+
 true_copy_tennis_NoNan["ace_cat"].hist()
 
 
@@ -100,11 +132,12 @@ some_data = true_copy_tennis_NoNan.iloc[:5]
 some_labels = true_copy_tennis_NoNan_labels.iloc[:5]
 some_data_prepared = full_pipeline.transform(some_data)
 print("예측:", lin_reg.predict(some_data_prepared))
+
 print("레이블:", list(some_labels))
 
-
-# Linear Regression 분석 결과
+## Linear Regression 분석 결과
 from sklearn.metrics import mean_squared_error
+
 tennis_predictions = lin_reg.predict(tennis_prepared)
 lin_mse = mean_squared_error(true_copy_tennis_NoNan_labels, tennis_predictions)
 lin_rmse = np.sqrt(lin_mse)
@@ -115,11 +148,11 @@ true_copy_tennis_NoNan_labels.hist()
 
 # Decision Tree Regressor 분석
 from sklearn.tree import DecisionTreeRegressor
+
 tree_reg = DecisionTreeRegressor(random_state=42)
 tree_reg.fit(tennis_prepared, true_copy_tennis_NoNan_labels)
 
-
-# Decision Tree Regressor 분석 결과
+## Decision Tree Regressor 분석 결과
 tennis_predictions = tree_reg.predict(tennis_prepared)
 tree_mse = mean_squared_error(true_copy_tennis_NoNan_labels, tennis_predictions)
 tree_rmse = np.sqrt(tree_mse)
@@ -128,11 +161,12 @@ tree_rmse
 
 # Random Forest 분석
 from sklearn.ensemble import RandomForestRegressor
+
 forest_reg = RandomForestRegressor(n_estimators=100, random_state=42)
 forest_reg.fit(tennis_prepared, true_copy_tennis_NoNan_labels)
 
 
-# Random Forest 분석 결과
+## Random Forest 분석 결과
 tennis_predictions = forest_reg.predict(tennis_prepared)
 forest_mse = mean_squared_error(true_copy_tennis_NoNan_labels, tennis_predictions)
 forest_rmse = np.sqrt(forest_mse)
